7bf50dab96a6511f53bdf4c801e0905f8002e442
benny5609 <benny1315@163.com>
2022-12-31 22:49:34 +0800

    enum log

---
+++ semi-structured-logs/src/lib.rs
@@ -0,0 +1,29 @@
+// This stub file contains items that aren't used yet; feel free to remove this module attribute
+// to enable stricter warnings.
+#![allow(unused)]
+
+/// various log levels
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub enum LogLevel {
+    Info,
+    Warning,
+    Error,
+}
+/// primary function for emitting logs
+pub fn log(level: LogLevel, message: &str) -> String {
+    let result = match level {
+        LogLevel::Info => info(message),
+        LogLevel::Warning => warn(message),
+        LogLevel::Error => error(message),
+    };
+    result
+}
+pub fn info(message: &str) -> String {
+    format!("[INFO]: {}", message)
+}
+pub fn warn(message: &str) -> String {
+    format!("[WARNING]: {}", message)
+}
+pub fn error(message: &str) -> String {
+    format!("[ERROR]: {}", message)
+}
